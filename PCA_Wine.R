904cdc028c391b68b3841add64847938e4ff1957
Nilayy7 <62058716+Nilayy7@users.noreply.github.com>
2020-03-31 09:39:13 +0530

    Update PCA_Wine.R

--- PCA_Wine.R
+++ PCA_Wine.R
@@ -65,3 +65,33 @@ kn<-kmeans.ani(norm_data,7)
 table(kn$cluster)
 
 
+###########Using Different Parameters######
+#using manhattan distance and average linkage
+
+dist2<-dist(norm_clust,method = "manhattan")
+fit2<-hclust(dist2,method="average") #Hierarchical clustering Using average linkage
+plot(fit2)
+
+rect.hclust(fit2, k=6, border="yellow")
+groups<-cutree(fit2,6)
+
+#Form the clusters
+cluster1 <-as.matrix(groups)
+View(cluster1)
+final1 <-cbind(cluster1,mydata)
+View(final1)
+
+#k means 
+library(plyr)
+norm_data1 <- scale(final1[,15:17])
+
+#Scree Plot 
+wss1 =(nrow(norm_data1)-1)*sum(apply(norm_data1, 2, var)) 
+for (i in 1:6) wss1[i] = sum(kmeans(norm_data1, centers=i)$withinss)
+plot(1:6, wss1, type="b", xlab="Number of Clusters", ylab="Sum of squares")
+
+#k means using k =6
+library(animation)
+ka <- kmeans.ani(norm_data1,6)
+table(ka$cluster)
+
